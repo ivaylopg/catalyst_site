65ea19283055e8c0fe8dda760a8ceeef898fd0f3
Ivaylo Getov <ivaylo.getov@gmail.com>
2014-09-09 23:28:25 +0200

    init

---
+++ js/ivaylo.js
@@ -0,0 +1,48 @@
+jQuery.browser = {};
+(function () {
+    jQuery.browser.msie = false;
+    jQuery.browser.version = 0;
+    if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
+        jQuery.browser.msie = true;
+        jQuery.browser.version = RegExp.$1;
+    }
+})();
+
+$(document).ready(function() {
+	var hoverBox = $("#logoHover");
+	hoverBox.css({"height":hoverBox.width(),"background":"none"});
+
+
+	$("#logoHover").hover(
+		function(){
+			$("#logo").attr('src','images/dopamine.svg');
+		},
+		function(){
+			$("#logo").attr('src','images/dopamineman.svg');
+		}
+	);
+
+	$(".hexFilled").attr("fill","#0000ff");
+	$(".svgHoneycomb").attr("width","40px");
+
+	$(".hexFilled").click(function(){
+		$(this).attr("class", "hexFilled shiny");
+	});
+
+	// Instead of .addClass("newclass")
+	//$("#item").attr("class", "oldclass newclass");
+	// Instead of .removeClass("newclass")
+	//$("#item").attr("class", "oldclass");
+
+	//$('#svgtest').svg({loadURL: "images/twohex.svg"}); 
+	//var svg = $('#svgtest').svg('get');
+	//svg.load("../images/twohex.svg"); 
+	//resetSize(svg);
+	
+
+});
+
+$(window).resize(function() {
+	var hoverBox = $("#logoHover");
+	hoverBox.css("height",hoverBox.width());
+});
