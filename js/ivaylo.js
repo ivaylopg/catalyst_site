fec67f4b8a4bcd758d2363d53295f8d2cf95199b
Ivaylo Getov <ivaylo.getov@gmail.com>
2014-09-14 17:15:59 +0200

    ...

--- js/ivaylo.js
+++ js/ivaylo.js
@@ -19,7 +19,9 @@ function loadDone(svg, error){
 		//$(this).children('img').removeClass("noShow");
 		$(this).append("<img class='svgImg' src=" + $(this).attr("data-source") + ".svg" + " />");
 		$(svg.root()).addClass("noShow");
-		$(this).css("height", "auto");
+		$(svg.root()).atrr("style","{width:100%; height: auto");
+		//$(this).css("height", "auto");
+		//console.log($(this));
 	} else {
 		if ($(this).attr("data-source").indexOf("honeycomb") >= 0) {
 			hcLoaded = true;
